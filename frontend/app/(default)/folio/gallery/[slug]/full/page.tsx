622b750b2b7de4140b8983784d3e55d355e3c8b9
Daniel Fernandes <ddanfernan@gmail.com>
2025-10-01 17:37:10 -0400

    Add centerfold

--- frontend/app/(default)/folio/gallery/[slug]/full/page.tsx
+++ frontend/app/(default)/folio/gallery/[slug]/full/page.tsx
@@ -38,6 +38,15 @@ export default async function FullShootPage({ params }: Props) {
   if (!folio?._id) return notFound()
 
   return (
+    <>
+        <div className="fixed inset-0 z-20 md:hidden pointer-events-none">
+          <div className="w-full h-full bg-[url('/images/centerfoldmobile.png')] bg-cover bg-center" />
+        </div>
+        {/* Medium Centerfold Image */}
+        <div className="md:fixed md:inset-0 md:z-20 md:pointer-events-none">
+          <div className="md:w-auto md:h-screen md:bg-[url('/images/centerfoldmedium.png')] md:bg-center md:bg-no-repeat md:bg-contain" />
+        </div>
+ 
     <div className="relative min-h-screen">
       <div className="p-6 md:p-12">
 
@@ -48,5 +57,6 @@ export default async function FullShootPage({ params }: Props) {
         )}
       </div>
     </div>
+       </>
   )
 }
