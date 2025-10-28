415f425c757689944d08d198983e7a77094cbac4
Daniel Fernandes <ddanfernan@gmail.com>
2025-10-28 10:40:19 -0400

    Update mobile splash page

--- frontend/app/(default)/page.tsx
+++ frontend/app/(default)/page.tsx
@@ -5,6 +5,7 @@ import {PortableText} from '@portabletext/react'
 import {AllPosts} from '@/app/components/Posts'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
+import Image from 'next/image'
 
 export default async function Page() {
   const {data: settings} = await sanityFetch({
@@ -13,18 +14,42 @@ export default async function Page() {
 
   return (
     <>
-      <div className="relative">
-        <div className="relative">
-          <div className="w-full h-full absolute top-0"></div>
+      <div className="relative grid grid-rows-4 grid-cols-6 h-screen w-screen pt-20 pb-16 overflow-hidden">
+        <div className='col-span-4 row-start-1 col-start-1'>
+          <Image
+            src="/images/cover01.png"
+            alt="Cover Image 1"
+            width={500}
+            height={500}
+            className="object-contain -rotate-5 -translate-x-7 scale-110 drop-shadow-lg/50"
+          />
         </div>
-        <div className=" flex flex-col items-center">
+        <div className='col-span-4 row-start-1 col-start-3'>
+          <Image
+            src="/images/cover02.png"
+            alt="Cover Image 2"
+            width={500}
+            height={500}
+            className="object-contain scale-110 rotate-5 translate-x-12 drop-shadow-lg/50"
+          />
         </div>
-      </div>
-      <div className="border-t border-gray-100 bg-gray-50">
-        <div className="container">
-          {/* <aside className="py-12 sm:py-20">
-            <Suspense>{await AllPosts()}</Suspense>
-          </aside> */}
+        <div className='col-span-4 col-start-1 row-start-3 z-30'>
+          <Image
+            src="/images/cover04.png"
+            alt="Cover Image 4"
+            width={500}
+            height={500}
+            className="object-contain -rotate-5 -translate-x-7 -translate-y-7 scale-110 z-30 drop-shadow-lg/50"
+          />
+        </div>
+        <div className='col-span-4 col-start-3 row-start-3 z-20'>
+          <Image
+            src="/images/cover03.png"
+            alt="Cover Image 3"
+            width={500}
+            height={500}
+            className="object-contain scale-110 rotate-15 translate-x-16 -translate-y-8 z-20 drop-shadow-lg/50"
+          />
         </div>
       </div>
     </>
