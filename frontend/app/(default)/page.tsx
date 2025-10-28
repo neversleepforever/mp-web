927c7546df24fac7936a58db0a106afaa3861614
Daniel Fernandes <ddanfernan@gmail.com>
2025-10-28 12:04:56 -0400

    Desktop splash page

--- frontend/app/(default)/page.tsx
+++ frontend/app/(default)/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
             alt="Cover Image 1"
             width={500}
             height={500}
-            className="object-contain -rotate-5 -translate-x-7 xl:translate-x-0 scale-110 xl:scale-125 md:scale-90 drop-shadow-lg/50"
+            className="object-contain -rotate-5 -translate-x-7 xl:translate-x-0 xl:translate-y-8 scale-110 xl:scale-115 md:scale-90 drop-shadow-lg/50"
           />
         </div>
         <div className='xl:flex xl:items-center xl:justify-center col-span-4 xl:col-span-1 row-start-1 col-start-3 xl:col-start-2 xl:row-start-1'>
@@ -30,7 +30,7 @@ export default async function Page() {
             alt="Cover Image 2"
             width={500}
             height={500}
-            className="object-contain scale-110 md:scale-90 xl:scale-125 rotate-5 translate-x-12 xl:translate-x-0 drop-shadow-lg/50"
+            className="object-contain scale-110 md:scale-90 xl:scale-115 rotate-5 xl:rotate-0 translate-x-12 xl:translate-x-0 xl:-translate-y-8 drop-shadow-lg/50"
           />
         </div>
         <div className='xl:flex xl:items-center xl:justify-center col-span-4 xl:col-span-1 col-start-1 row-start-3 xl:col-start-3 xl:row-start-1 z-30'>
@@ -39,7 +39,7 @@ export default async function Page() {
             alt="Cover Image 4"
             width={500}
             height={500}
-            className="object-contain -rotate-5 -translate-x-7 xl:translate-x-0 -translate-y-7 xl:translate-y-0 scale-110 md:scale-90 xl:scale-125 z-30 drop-shadow-lg/50"
+            className="object-contain -rotate-5 xl:rotate-0 -translate-x-7 xl:translate-x-0 -translate-y-7 xl:translate-y-4 scale-110 md:scale-90 xl:scale-115 z-30 drop-shadow-lg/50"
           />
         </div>
         <div className='xl:flex xl:items-center xl:justify-center col-span-4 xl:col-span-1 col-start-3 row-start-3 xl:col-start-4 xl:row-start-1 z-20'>
@@ -48,7 +48,7 @@ export default async function Page() {
             alt="Cover Image 3"
             width={500}
             height={500}
-            className="object-contain scale-110 md:scale-90 xl:scale-125 rotate-15 xl:rotate-0 translate-x-16 xl:translate-x-0 -translate-y-8 xl:translate-y-0 z-20 drop-shadow-lg/50"
+            className="object-contain scale-110 md:scale-90 xl:scale-115 rotate-15 xl:rotate-8 translate-x-16 xl:translate-x-0 -translate-y-8 xl:translate-y-0 z-20 drop-shadow-lg/50"
           />
         </div>
       </div>
