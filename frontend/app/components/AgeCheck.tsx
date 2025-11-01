f9f58910e24c3514f1bc61e346d145f3f803604e
Daniel Fernandes <ddanfernan@gmail.com>
2025-11-01 18:34:05 -0400

    enable age check

--- frontend/app/components/AgeCheck.tsx
+++ frontend/app/components/AgeCheck.tsx
@@ -3,10 +3,12 @@ import { useState, useEffect } from "react"
 
 export default function AgeCheck({ children }: { children: React.ReactNode }) {
   const [verified, setVerified] = useState(false)
+  const [loaded, setLoaded] = useState(false)
 
   useEffect(() => {
     const isVerified = localStorage.getItem("age-verified")
     if (isVerified === "true") setVerified(true)
+    setLoaded(true)
   }, [])
 
   const handleVerify = () => {
@@ -14,14 +16,18 @@ export default function AgeCheck({ children }: { children: React.ReactNode }) {
     setVerified(true)
   }
 
+  if (!loaded) {
+    return null
+  }
+
   return (
     <div className="relative">
       {children}
       {!verified && (
-        <div className="fixed inset-0 h-full w-full z-40 flex items-center justify-center backdrop-blur-lg z-[9999]">
-          <div className="bg-black z-[9999]  text-white text-center w-full py-5 px-1 font-display uppercase">
+        <div className="fixed inset-0 h-full w-full flex items-center justify-center backdrop-blur-lg z-[9999]">
+          <div className="bg-black text-white text-center w-full py-5 px-1 font-display uppercase">
             <h2 className="text-xl">
-              The following content is for 18+ adults only –{" "}
+              The following content is for 18+ adults only —{" "}
               <button
                 onClick={handleVerify}
                 className="uppercase underline decoration-1 hover:decoration-2 transition-all cursor-pointer"
