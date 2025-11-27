30b4249798a225f610926ddc3ca9fdf800e286dc
Daniel Fernandes <ddanfernan@gmail.com>
2025-11-27 10:15:09 -0500

    mobile draggable

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,8 +8,8 @@ export default async function Page() {
   if (!home) return null
 
   return (
-    <TextDistortFilter>
-      <div className="overscroll-none relative grid grid-rows-4 md:grid-rows-5 xl:grid-rows-1 grid-cols-6 xl:grid-cols-4 h-screen w-screen pt-20 md:pt-8 xl:pt-16 pb-16 overflow-hidden">
+    <TextDistortFilter className='overscroll-none'>
+      <div className="overscroll-none relative grid grid-rows-4 md:grid-rows-5 xl:grid-rows-1 grid-cols-6 xl:grid-cols-4 h-screen max-h-screen w-screen pt-20 md:pt-8 xl:pt-16 pb-16 overflow-hidden">
         <DraggableImages home={home} />
       </div>
     </TextDistortFilter>
