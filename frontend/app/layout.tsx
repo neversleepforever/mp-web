8f35f9b80278a69437dbe66c0a5c0763759f7320
Daniel Fernandes <ddanfernan@gmail.com>
2025-12-01 15:12:15 -0500

    try fixing viewport issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,38 +1,31 @@
 "use client"
 
 import "./globals.css"
-
 import Footer from "@/app/components/Footer"
 import Header from "@/app/components/Header"
 import AgeCheck from "./components/AgeCheck"
 import Centerfold from "./components/Centerfold"
 
-import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation"
 
-
-export default function BaseLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-    const pathname = usePathname();
+export default function BaseLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
 
   const isDark =
     pathname?.startsWith("/about") ||
     pathname?.startsWith("/contact") ||
-    pathname?.startsWith("/services");
+    pathname?.startsWith("/services")
 
   return (
-    <html
-      lang="en"
-      className={`overscroll-none`}
-    >
-      <body className={`${isDark ? "dark" : "" } bg-white dark:bg-black overscroll-none` }>
+    <html lang="en" className="overscroll-none">
+      <body className={`${isDark ? "dark" : ""} bg-white dark:bg-black overscroll-none`}>
         <Centerfold />
         <AgeCheck>
-          <section className="min-h-screen overscroll-none">
+          <section
+            className="flex flex-col min-h-[100dvh] overflow-hidden"
+          >
             <Header />
-            <main className="overscroll-none">{children}</main>
+            <main className="flex-1 overflow-hidden">{children}</main>
             <Footer />
           </section>
         </AgeCheck>
