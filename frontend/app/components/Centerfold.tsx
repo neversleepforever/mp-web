0548a5470abb8f12dbe3a65def2fec7834c5c46e
Daniel Fernandes <ddanfernan@gmail.com>
2025-12-01 20:26:24 -0500

    test

--- frontend/app/components/Centerfold.tsx
+++ frontend/app/components/Centerfold.tsx
@@ -12,12 +12,17 @@ export default function Centerfold() {
     <>
       {/* Mobile staples */}
       <div className="fixed top-0 left-0 w-full h-[100dvh] md:hidden pointer-events-none z-50 overscroll-contain">
-        <FadeInImage
-          src="/images/mobilestaples.png"
-          alt=""
-          fill
-          className="object-contain"
-        />
+        <div className="relative w-full h-full">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <FadeInImage
+              src="/images/mobilestaples.png"
+              alt=""
+              width={800}  // adjust width/height as needed
+              height={1600}
+              className="max-h-[100dvh] max-w-full object-contain"
+            />
+          </div>
+        </div>
       </div>
 
       {/* Shadow overlay */}
@@ -31,7 +36,7 @@ export default function Centerfold() {
       </div>
       
       {/* Desktop staples */}
-<div className="md:fixed md:top-0 md:left-0 md:w-full md:h-[100dvh] md:pointer-events-none z-50">
+{/* <div className="md:fixed md:top-0 md:left-0 md:w-full md:h-[100dvh] md:pointer-events-none z-50">
   <div className="hidden md:block relative md:h-[100dvh]">
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <FadeInImage
@@ -43,7 +48,16 @@ export default function Centerfold() {
       />
     </div>
   </div>
-</div>
+
+</div> */}
+
+  <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-50">
+    <FadeInImage
+      src="/images/staples.png"
+      alt=""
+      className="max-h-full max-w-full object-contain"
+    />
+  </div>
 
     </>
   )
